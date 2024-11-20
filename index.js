cffc22e3f7d22ecd5b5fed416e36ce30d6fb9aec
Bhagyashree <bshree2604@gmail.com>
2024-11-20 16:27:47 +0530

    add pause and resume logic to buttons

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 const sayitBtn = document.getElementById("btn")
 const text = document.getElementById("text")
+const pause=document.getElementById("pausebtn")
+const resume=document.getElementById("resumebtn")
 
 function speakthetext(e) {
     const utterence = new SpeechSynthesisUtterance(text.value)
+    utterence.onend=(event)=>{
+        alert("Speech ended")
+    }
     speechSynthesis.speak(utterence)
 }
+function pausefn(){
+    speechSynthesis.pause();
+}
+function resumefn(){
+    speechSynthesis.resume();
+}
 sayitBtn.addEventListener("click", speakthetext);
+pause.addEventListener("click",pausefn)
+resume.addEventListener("click",resumefn)
 
 
 
